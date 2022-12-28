b1554235a87175c434807ecbe38b048f00d31fbd
TannerEsch <eschmanntanner@gmail.com>
2022-12-28 00:00:58 -0500

    still trying to deploy. Client is runnning, server is not.

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+require('dotenv').config()
 
 let sequelize;
 if (config.use_env_variable) {
@@ -32,6 +33,5 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
 
 module.exports = db;
